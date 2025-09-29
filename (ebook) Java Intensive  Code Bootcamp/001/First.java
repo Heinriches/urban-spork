aae8df3a712bbe77e3110ca135a38f7f11ecb809
Heinriches <heydrich.institut@gmail.com>
2025-09-29 12:14:40 -0300

    Create First.java

---
+++ (ebook) Java Intensive  Code Bootcamp/001/First.java
@@ -0,0 +1,22 @@
+//package fun.sanjibsinha;
+
+import java.util.Scanner;
+
+public class First{
+	public static void main(String[] args){
+		// Declaring a String variable to hold user input
+		String inputString;
+		
+		// Creating a Scanner object to read input from the console (System.in)
+		Scanner consoleIn = new Scanner(System.in);
+		
+		// Prompting the user for input
+		System.out.println("Enter a string: ");
+		
+		// Reading the entire line of input from the console
+		inputString = consoleIn.nextLine();
+		
+		// Displaying the inut back to the user
+		System.out.println("You entered string: " +inputString);
+	}
+}
